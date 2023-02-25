dc46c33eb9b49412f480e2d05393b4c29750e84c
jh_ux <jonathanhernandez.ux@gmail.com>
2023-02-24 19:27:06 -0500

    Fix: folder structure

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ connectDB();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-const port =  process.env.PORT || 8082;
+const port =  process.env.PORT || 3000;
 
 app.listen(port, ()=> console.log(`Server running on port ${port}`));
